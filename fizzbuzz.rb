710220f9859a26ecf55e013fd7506ccdc6c289fc
Ryan Melchior <ryan.m2885@gmail.com>
2021-03-08 17:35:07 -0800

    done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+    return "FizzBuzz" if fifteen(int)
+    return "Buzz" if five(int)
+    return "Fizz" if three(int)
+end
+
+def fifteen(int)
+    int % 15 == 0
+end
+
+def five(int)
+    int % 5 == 0
+end
+
+def three(int)
+    int % 3 ==0
+end
